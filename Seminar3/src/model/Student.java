2c705b70276947959798d7c96da7cc9e88b74acf
TACO_LAPTOP\marku <s23harimark@venta.lv>
2026-02-25 11:23:07 +0200

    Added constructor

--- Seminar3/src/model/Student.java
+++ Seminar3/src/model/Student.java
@@ -28,13 +28,45 @@ public class Student {
 
 	}
 	public void setName(String inputName) {
-		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}( {1}[A-Z]{1}[a-z]{2,15})?"))) {
+		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([]{1}[A-Z]{1}[a-z]{2,15})?"))) {
 			name = inputName;
+		}
+		else{
+			name = "Unknown";
+		}
+	}
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (!inputSurname.isEmpty()) && (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([-]{1}[A-Z]{1}[a-z]{2,15})?"))) {
+			surname = inputSurname;
+		}
+		else{
+			surname = "Unknown";
+		}
+	}
+	public void setPersonCode(String inputPersonCode) {
+		if((inputPersonCode != null) && (!inputPersonCode.isEmpty()) && (inputPersonCode.matches("[0-9]{6}[-]{1}[0-9]{5}"))) {
+			personCode = inputPersonCode;
+		}
+		else{
+			personCode = "Unknown";
 		}
 	}
 
+
 	//4.1 bezargumentu konstruktors
+	public Student(){
+		setStudId();
+		setName("Janis");
+		setSurname("Berzins");
+		setPersonCode("123456-12345");
+	}
 	//4.2 argumentiem konstruktors
+	public Student(String inputName, String inputSurname, String inputPersonCode){
+		setStudId();
+		setName(inputName);
+		setSurname(inputSurname);
+		setPersonCode(inputPersonCode);
+	}
 	//5. toString funkcija
 	//6. visas parejas funkcijas pec nepieciesibas
 
