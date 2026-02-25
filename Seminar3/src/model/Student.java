8fb4ef014e39255855b709af68c4618a78e9c207
TACO_LAPTOP\marku <s23harimark@venta.lv>
2026-02-25 11:17:10 +0200

    Added set class

--- Seminar3/src/model/Student.java
+++ Seminar3/src/model/Student.java
@@ -1,10 +1,42 @@
 package model;
-
+//modelu klase
 public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//1. mainigie
+	private long studId;
+	private String name;
+	private String surname;
+	private String personCode;
+	//palig mainigais, kam nevajag ne set ne get
+	private static long counter = 0;
+	//2. get funkcija
+	public long getStudId() {
+		return studId;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getPersonCode() {
+		return personCode;
+	}
+	//3. set funkcija
+	public void setStudId(){
+		studId = counter;
+		counter++;
 
 	}
+	public void setName(String inputName) {
+		if((inputName != null) && (!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{2,15}( {1}[A-Z]{1}[a-z]{2,15})?"))) {
+			name = inputName;
+		}
+	}
 
+	//4.1 bezargumentu konstruktors
+	//4.2 argumentiem konstruktors
+	//5. toString funkcija
+	//6. visas parejas funkcijas pec nepieciesibas
+
+	
 }
